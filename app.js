9bddc34d59bcf3acd3355aa6c2f50cc038ee3d23
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-09-30 16:50:38 +0300

    third-commit

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const http = require('http');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-app.use();
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"></button></form>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">click Me</button></form>')
 })
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+})
 //the use helps us to add a middleware function
 app.use((req, res, next) => {
     console.log("in the middleware");
