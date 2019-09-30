5e5e9cf6033c942a46eeb7efffeac2dafce72b99
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-09-30 16:29:28 +0300

    second-commit

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const app = express();
 
+app.use();
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"></button></form>')
+})
+
 //the use helps us to add a middleware function
 app.use((req, res, next) => {
     console.log("in the middleware");
