45cc3e4a13b733eda91481dc83f3d81697589177
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-03 17:43:14 +0300

    ninth-commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
 const Router = express.Router();
-
 const rootDir = require('../util/path');
 
-Router.get('/add-product', (req, res, next) => {
+const products = [];
+
+Router.get('/addproduct', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'addproduct.html'));
 });
 
 Router.post('/product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect("/");
 });
 
-module.exports = Router;
+exports.routes = Router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
-
+const adminData = require('./admin');
 
 //the use helps us to add a middleware function
 router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(__dirname,  '../','views', 'shop.html'));
-
 });
 module.exports = router;
+
