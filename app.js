3c40f4f18b902368e763fc22007c53a81d9fd3f0
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-11 15:02:31 +0300

    pug-setup

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
